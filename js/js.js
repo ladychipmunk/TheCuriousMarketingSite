1652c9a518508a737f748964927a0e6519cb305d
webdesign account <webdesign@dublindesign.ie>
2017-05-11 18:35:09 +0100

    mobile menu workin, add blog to footer

--- js/js.js
+++ js/js.js
@@ -2,3 +2,8 @@ $(document).ready(function(){
 	$('.slider').slider({ height:800 });
 });
          
+ // Initialize collapse button
+  $(".button-collapse").sideNav();
+  // Initialize collapsible (uncomment the line below if you use the dropdown variation)
+  //$('.collapsible').collapsible();
+        
